5345dc047708134b8571e0b0016f6cbd8f44c59a
Jhonw1 <bftpgjhonwilmar@gmail.com>
2024-03-11 08:04:21 -0500

    actualizado

--- app.js
+++ app.js
@@ -6,14 +6,25 @@ const validateToken = require('./src/routes/GetAuthentication');
 const auth = require('./src/routes/getJwt');
 const login = require('./src/routes/login');
 const register = require('./src/routes/register');
+const gruasRoutes = require('./src/routes/gruasRutes');
+const getGruasInfoRoute = require('./src/routes/getGruasInfo');
 const cors = require('cors');
-// const getUserInfo = require('./routes/getUserInfo');
 
 const app = express().use(cors()).use(bodyParser.json()).use(cookieParser(signingKey.SIGNING_KEY_COOKIE));
+
+// Establece la ruta base de las imágenes
+app.locals.rutaBaseImagenes = '/ruta-base-imagenes'; // Cambia esto según tus necesidades
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
 app.use('/register', register);
 app.use('/login', login);
 app.use('/readToken', validateToken);
-// app.use('/getInfo', getUserInfo);
+app.use('/gruas', gruasRoutes);
+app.use('/getGruasInfo', getGruasInfoRoute);
 
 const PORT = process.env.PORT ?? 3000;
 
--- src/data/datas.js
+++ src/data/datas.js
@@ -15,55 +15,84 @@ database.connect();
 
 class userData {
     static addClient(user, callback) {
-        console.log(user)
-        const query = "INSERT INTO clients ( user, email, password, phone ) VALUES (  ?, ?, ?, ?)";
-        database.query(query, [user.user, user.email, user.password, user.phone], (err, result) => {
-            if (err) {
-                console.error("Error registered " + err.message);
-                callback(err, null);
-            } else {
-                console.log("Client registered successfully");
-                callback(null, true);
-            }
-        });
+      const query = "INSERT INTO clients (user, email, password, phone) VALUES (?, ?, ?, ?)";
+      database.query(query, [user.user, user.email, user.password, user.phone], (err, result) => {
+        if (err) {
+          console.error("Error al registrar el cliente: " + err.message);
+          callback(err, false);
+        } else {
+          const nuevoClienteId = result && result.insertId;
+          console.log("Cliente registrado exitosamente. ID: " + nuevoClienteId);
+          callback(null, true, nuevoClienteId);
+        }
+      });
     }
-
-    // static obtenerInformacionUsuario(id, callback) {
-    //     const query = 'SELECT * FROM Clients WHERE id = ?';
-    //     database.query(query, [id], (err, result) => {
-    //         if (err) {
-    //             console.error('Error al obtener la información del usuario: ' + err.message);
-    //             callback(err, null);
-    //         } else if (result.length === 0) {
-    //             callback(null, null);
-    //         } else {
-    //             const usuario = result[0];
-    //             callback(null, usuario);
-    //         }
-    //     });
-    // }
-
+    static addGrua(grua, clienteId, callback) {
+      const query = "INSERT INTO Gruas (marca, modelo, capacidad, foto_path, cliente_id ) VALUES (?, ?, ?, ?, ?)";
+      const values = [grua.marca, grua.modelo, grua.capacidad, grua.foto_path, clienteId];
+    
+      database.query(query, values, (err, result) => {
+        if (err) {
+          console.error("Error al agregar la grúa: " + err.message);
+          callback(err, false);  // Enviar false en caso de error
+        } else {
+          const nuevaGruaId = result && result.insertId;
+          console.log("Grúa agregada exitosamente. ID: " + nuevaGruaId);
+          callback(null, true, nuevaGruaId);  // Enviar true y la nuevaGruaId en caso de éxito
+        }
+      });
+    }
+  
+    static obtenerInformacionUsuario(id, callback) {
+      const query = 'SELECT * FROM Clients WHERE id = ?';
+      database.query(query, [id], (err, result) => {
+        if (err) {
+          console.error('Error al obtener la información del usuario: ' + err.message);
+          callback(err, null);
+        } else if (result.length === 0) {
+          callback(null, null);
+        } else {
+          const usuario = result[0];
+          callback(null, usuario);
+        }
+      });
+    }
+  
     static login(user, callback) {
-        const query = 'SELECT * FROM clients WHERE email = ? AND password = ?';
-        database.query(query, [user.email, user.password], (err, result) => {
-            if (err) {
-                console.error('Error al autenticar el usuario: ' + err.message);
-                callback(err, null);
-            } else if (result.length === 0) {
-                callback(null, null);
-            } else {
-                const user = result[0];
-                callback(null, user);
-            }
-        });
+      const query = 'SELECT * FROM clients WHERE email = ? AND password = ?';
+      database.query(query, [user.email, user.password], (err, result) => {
+        if (err) {
+          console.error('Error al autenticar el usuario: ' + err.message);
+          callback(err, null);
+        } else if (result.length === 0) {
+          callback(null, null);
+        } else {
+          const user = result[0];
+          callback(null, user);
+        }
+      });
     }
-}
-
-
-
-
-
-
-module.exports = {
+  
+    static obtenerGruas(callback) {
+      const query = 'SELECT * FROM Gruas';
+  
+      database.query(query, (err, result) => {
+        if (err) {
+          console.error('Error al obtener grúas desde la base de datos:', err.message);
+          callback(err, null);
+        } else {
+          callback(null, result);
+        }
+      });
+    }
+  
+    
+  
+  }
+  
+  
+  
+  module.exports = {
     userData
-}
+  };
+  
